145c3e69ac2c5a3e97836ea893e83f66c41de7c8
harshit singh <harshitsingh1158@gmail.com>
2026-04-26 20:01:54 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname)); // serve frontend
+
+// 🔐 ADD LOGIN HERE
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === "admin" && password === "1234") {
+        res.json({ message: "Login successful" });
+    } else {
+        res.status(401).json({ message: "Invalid credentials" });
+    }
+});
+
+
+let ideas = [];
+
+if (fs.existsSync("data.json")) {
+  ideas = JSON.parse(fs.readFileSync("data.json"));
+}
+
+
+app.post("/add", (req, res) => {
+  ideas.push(req.body);
+
+  // SAVE TO FILE
+  fs.writeFileSync("data.json", JSON.stringify(ideas, null, 2));
+
+  res.json({ message: "Idea saved successfully" });
+});
+
+
+app.get("/ideas", (req, res) => {
+  res.json(ideas);
+});
+
+app.get("/getIdeas/:domain", (req, res) => {
+    const domain = req.params.domain;
+
+    const filtered = ideas.filter(i => i.domain === domain);
+
+    res.json(filtered);
+});
+
+app.get("/status", (req, res) => {
+    res.json({
+        status: "Server is running",
+        time: new Date().toLocaleString()
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
